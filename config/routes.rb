6d545f520735d8f83acc99624d588169bbbcccf9
Larry Reid <lcreid@jadesystems.ca>
2018-03-20 15:39:36 -0700

    Get rid of unneeded controllers and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  patch "children/:id/edit", to: "children#update", as: :children
-  patch "parents/:id/edit", to: "parents#update", as: :parents
   resources :people
 end
