cce3b1c031a658613b5d4c7e8eb1e5bc0ff0cbae
Larry Reid <lcreid@jadesystems.ca>
2018-03-21 08:26:31 -0700

    Remove unneeded methods for addresses.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,11 +1,5 @@
 class Person < ApplicationRecord
   include Dag
 
-  def address_types
-    address_people.map(&:address_type).uniq
-  end
-
-  def to_json
-    pp as_json.merge(addresses: addresses.as_json).merge(phones: phones.as_json)
-  end
+  validates :name, presence: true
 end
