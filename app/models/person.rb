a295bfeec4e1c5de21c153a74106e9e60e97e508
Larry Reid <lcreid@jadesystems.ca>
2018-03-21 08:37:45 -0700

    Don't show current in parent or child list.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ApplicationRecord
   include Dag
 
+  scope :all_except, -> (person) { where.not(id: person.is_a?(Person) ? person.id : person) }
+
   validates :name, presence: true
 end
