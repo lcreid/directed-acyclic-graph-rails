54170afd36d69e4e26e7a11e66f94656ceaa6607
Larry Reid <lcreid@jadesystems.ca>
2018-03-28 16:37:00 -0700

    Remove unnecessary code.

--- app/assets/javascripts/submit-on-change.js
+++ app/assets/javascripts/submit-on-change.js
@@ -1,13 +1,5 @@
 $(document).on('turbolinks:load', function(e) {
-  console.log('turbolinks:load');
   $('form.js-submit-on-change').change(function(event) {
-    console.log('Form auto-submitting');
-    // $("body").prepend('<div class="spinner"></div>');
-    // console.log('Spinner shown');
-    // $(document).ajaxComplete(function(e){
-      // console.log('Complete');
-      // $(".spinner").remove();
-    // });
     $(this).submit();
   });
 });
